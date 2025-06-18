32fd67cc8ca5936a0e8bc24e38516ba5e6b91215
Jason Fillbach <Jasonfillbach@gmail.com>
2025-06-18 09:49:17 -0700

    Add files via upload
    First Rendition pulling 6 years of AAPL Balance Sheets - WIP

---
+++ sec_10k_parser/config.py
@@ -0,0 +1,12 @@
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/114.0.0.0 Safari/537.36 "
+                  "(Chromium@gmail.com)",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive",
+    "Referer": "https://www.sec.gov/"
+}
+CIK = '0000320193'  # Apple Inc.
+BASE_URL = f'https://data.sec.gov/submissions/CIK{CIK}.json'
